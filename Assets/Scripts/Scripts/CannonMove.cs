b00cc843168e5fe4c4ba104c92709430add96ce1
SandyLinGitHub <91555784+SandyLinGitHub@users.noreply.github.com>
2024-05-09 14:59:17 -0400

    Add files via upload

--- Assets/Scripts/Scripts/CannonMove.cs
+++ Assets/Scripts/Scripts/CannonMove.cs
@@ -5,59 +5,72 @@ using UnityEngine;
 public class CharacterControllerScript : MonoBehaviour
 {
     public float speed = 5.0f;
-    public Transform camera = null;
-    public float cameraPos_LF = 0.0f;
+    public Transform cameraRig = null;
+    //public float cameraPos_LF = 0.0f;
     public float mouseSense = 3.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         //player = GetComponent<CharacterController>();
-        camera = GameObject.Find("Main Camera").transform;
+        cameraRig = GameObject.Find("OVRCameraRig").transform;
 
         //locks the cursor in place st. when the camera moves, the cursor doesn't move
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
 
         //makes the mouse invisible
-        Cursor.visible = false;
+        //Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Mouse direction
-        Vector2 mouseDirection = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        
+        //Vector2 mouseDirection = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
         //camera position is based on the y axis
-        cameraPos_LF = cameraPos_LF - mouseDirection.y * mouseSense;
-        
+        //cameraPos_LF = cameraPos_LF - mouseDirection.y * mouseSense;
+
         //The following if statement basicaly does the clamping, but manually
-        if (cameraPos_LF < -90)
-        {
-            cameraPos_LF = -90;
-        }
-        else if (cameraPos_LF > 90)
-        {
-            cameraPos_LF = 90;
-        }
+        //if (cameraPos_LF < -90)
+        //{
+        //    cameraPos_LF = -90;
+        //}
+        //else if (cameraPos_LF > 90)
+        //{
+        //    cameraPos_LF = 90;
+        //}
 
         //camera.localEulerAngles = Vector3.right * cameraPos;
-        transform.Rotate(Vector3.up * mouseDirection.x * mouseSense);
+        //transform.Rotate(Vector3.up * mouseDirection.x * mouseSense);
 
         //Character direction
-        Vector2 movingDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        
-        //Normalize transforms the vector st. it goes from -1 to 1 on all three axis
-        movingDir.Normalize();
+        //Vector2 movingDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.Rotate(50.0f * Time.deltaTime, 0.0f, 0.0f);
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            transform.Rotate(-50.0f * Time.deltaTime, 0.0f, 0.0f);
-        }
+        //Normalize transforms the vector st. it goes from -1 to 1 on all three axis
+        //movingDir.Normalize();
+
+        //if (Input.GetKeyDown(KeyCode.UpArrow))
+        //{
+        //    transform.Rotate(50.0f * Time.deltaTime, 0.0f, 0.0f);
+        //}
+        //if (Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    transform.Rotate(-50.0f * Time.deltaTime, 0.0f, 0.0f);
+        //}
+
+        // Get the input from the VR headset
+        Vector2 headsetDirection = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+        // Rotate the character controller based on headset rotation
+        float rotationY = cameraRig.localEulerAngles.y;
+        transform.rotation = Quaternion.Euler(0, rotationY, 0);
+
+        // Calculate movement direction based on headset direction
+        Vector3 movement = new Vector3(headsetDirection.x, 0.0f, headsetDirection.y) * speed * Time.deltaTime;
+
+        // Move the character controller
+        transform.Translate(movement, Space.Self);
     }
 }
 
--- Assets/Scripts/Scripts/shoot.cs
+++ Assets/Scripts/Scripts/shoot.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Lab3Code : MonoBehaviour
 {
     public GameObject Barrel;
-    public GameObject Ball;
+    public GameObject Bullet;
     public GameObject OVRCameraRig;
     // Start is called before the first frame update
     void Start()
@@ -16,12 +16,29 @@ public class Lab3Code : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (UnityEngine.Input.GetMouseButtonDown(0))
-        {
-            GameObject newBullet = Instantiate(Ball, Barrel.transform.position, Barrel.transform.rotation);
-            newBullet.GetComponent<Rigidbody>().AddForce(transform.up * 1000);
+        //if (UnityEngine.Input.GetMouseButtonDown(0))
+        //{
+        //    GameObject newBullet = Instantiate(Bullet, Barrel.transform.position, Barrel.transform.rotation);
+        //    newBullet.GetComponent<Rigidbody>().AddForce(transform.up * 1000);
 
-            Destroy(newBullet, 4);
+        //      Destroy(newBullet, 4);
+        //}
+
+        // Check if the A button on the right Oculus Touch controller is pressed
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryThumbstickDown, OVRInput.Controller.RTouch))
+        {
+            ShootBullet();
         }
+    }
+    void ShootBullet()
+    {
+        // Instantiate a new bullet at the barrel's position and rotation
+        GameObject newBullet = Instantiate(Bullet, Barrel.transform.position, Barrel.transform.rotation);
+
+        // Add force to the bullet to shoot it out
+        newBullet.GetComponent<Rigidbody>().AddForce(transform.up * 1000);
+
+        // Destroy the bullet after 4 seconds
+        Destroy(newBullet, 4);
     }
 }
